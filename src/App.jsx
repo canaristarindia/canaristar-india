e836f60f4b3ce0e9519a873c7fc73df0c9ae4af8
keval-vachhani <kevalvachhani3008@gmail.com>
2025-12-18 09:17:49 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>Terms and Condition</h2>
+      <p>
+        By accessing and using this website, you agree to be bound by these
+        Terms and Conditions. All content and services provided on this website
+        are for lawful purposes only. You agree not to misuse the website or its
+        services. We reserve the right to modify, suspend, or discontinue any
+        part of the service at any time without prior notice. All payments made
+        through this website are processed securely via Razorpay. Violation of
+        these terms may result in termination of access to our services. If you
+        have any questions, contact us at: Email: support@canaristar.com
+      </p>
+      <h2>Refund & Cancellation Policy</h2>
+      <p>
+        Once a payment is completed, cancellation requests can be made within 24
+        hours of purchase. Refunds will be initiated only if the request is
+        valid and approved by us. Approved refunds will be processed within 5–7
+        working days and credited to the original payment method. For digital
+        products or services, refunds may not be applicable once the service has
+        been accessed or delivered. For refund-related queries, contact us at:
+        Email: support@canaristar.com
+      </p>
+      <h2>🚚 Shipping & Delivery Policy</h2>
+      <p>
+        We do not ship any physical products. All our products and services are
+        delivered digitally via email, user dashboard, or direct access on the
+        website. Delivery is usually completed instantly or within 24 hours
+        after successful payment. For delivery-related issues, contact us at:
+        Email: support@canaristar.com
+      </p>
+    </>
+  );
+}
+
+export default App;
